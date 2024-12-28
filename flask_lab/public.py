4e51612a18d38f72880964e7133a07ee58c71ae9
Chaim <gchaimke@gmail.com>
2024-12-28 13:04:24 +0200

    test copy matenim

--- flask_lab/public.py
+++ flask_lab/public.py
@@ -1,13 +1,6 @@
-from sqlite3 import OperationalError
-import sqlite3
 from urllib.parse import unquote
-from flask import (
-    Blueprint, app, current_app, flash, g, redirect, render_template, request, url_for
-)
-from werkzeug.exceptions import abort
+from flask import Blueprint, render_template, request
 
-from .auth import min_role_required
-from .db import init_db, insert_to_db, update_by_id, delete_by_id, get_by_id, get_join
 from . import const
 
 unique_not_found_urls = set()
@@ -16,6 +9,11 @@ bp = Blueprint('pablic', __name__)
 @bp.route('/')
 def index():
     return render_template('public/main.html')
+
+
+@bp.route('/matenim')
+def matenim():
+    return render_template('public/matenim.html')
 
 
 # Catch-all route for non-existing pages
@@ -30,4 +28,45 @@ def page_not_found(e):
         last_slash = last_slash[-1]
     # Respond with a custom message or redirect
     return render_template('public/main.html', requested_url=clean_url, last_slash=last_slash)
+
+@bp.route('/matenim/copy')
+def copy_matching_images():
+    import os
+    import re
+    import shutil
+    total = []
+    unique_file = set()
+    # Define source and destination folders
+    source_folder = f'{const.ROOT_PATH}/../wp-content/uploads'
+    destination_folder = f'{const.ROOT_PATH}/static/img/public/products/matenim'
+
+    # Define the pattern for the desired filenames
+    pattern = r"^.+-\d+\.\d+(V|v)-\d+\.\d+(A|a)-\d+(W|w)(-|_)?(1|2|3)?.(jpg|jpeg|JPG|JPEG|png|PNG)$"
+
+    # Ensure the destination folder exists
+    # if not os.path.exists(destination_folder):
+    #     os.makedirs(destination_folder)
+
+    # Compile the regular expression for matching filenames
+    regex = re.compile(pattern)
+
+    # Iterate through files in the source folder
+    for filename in os.listdir(source_folder):
+        if regex.match(filename):  # Check if the filename matches the pattern
+            # unique_file_name = filename.split('W_')[0]
+            # if unique_file_name in unique_file:
+            #     continue
+            # unique_file.add(unique_file_name)
+            source_path = os.path.join(source_folder, filename)
+            destination_path = os.path.join(destination_folder, filename)
+            msg = f"Copying: {filename}"
+            total.append(msg)
+
+            # # Copy the file
+            # shutil.copy(source_path, destination_path)
+            # print(f"Copied: {filename}")
+    return render_template('public/matenim.html', msg=total)
+
+
+
 
